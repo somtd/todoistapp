87e7f1612c3837c8f3d065acc668d0a5dec4310f
somtd <somtd1104@me.com>
2016-04-27 11:12:24 +0900

    adding slackapp

---
+++ slackapp.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import urllib
+import urllib2
+import os
+
+url = "https://slack.com/api/chat.postMessage"
+
+params = {'token': os.environ["SLACK_TOKEN"],
+          'channel': os.environ["SLACK_CHANNEL"],
+          'text': 'test post'
+          }
+
+params = urllib.urlencode(params)
+req = urllib2.Request(url)
+
+req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+req.add_data(params)
+res = urllib2.urlopen(req)
+
+body = res.read()
+
