25ba14b13f41af2c31260d65f39a8b1e5925fd36
somtd <somtd1104@me.com>
2016-04-27 12:04:19 +0900

    fix icon

--- slackapp.py
+++ slackapp.py
@@ -7,17 +7,15 @@ url = "https://slack.com/api/chat.postMessage"
 
 params = {'token': os.environ["SLACK_TOKEN"],
           'channel': os.environ["SLACK_CHANNEL"],
-          'username': 'Todoist Bot',
-          'icon_url': 'https://blog.todoist.com/wp-content/uploads/2015/09/todoist-logo.png',
+          'username': 'Todoist',
+          'icon_url': 'https://pbs.twimg.com/profile_images/644169103750512640/zWCOmZLI.png',
           'text': 'test post'
           }
 
 params = urllib.urlencode(params)
 req = urllib2.Request(url)
-
 req.add_header('Content-Type', 'application/x-www-form-urlencoded')
 req.add_data(params)
 res = urllib2.urlopen(req)
-
 body = res.read()
 
