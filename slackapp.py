1c8fd39f8ce8a65120e2eff38a4d5bb5569bce02
somtd <somtd1104@me.com>
2016-05-10 09:29:26 +0900

    edit slackapp

--- slackapp.py
+++ slackapp.py
@@ -11,11 +11,18 @@ params = {'token': os.environ["SLACK_TOKEN"],
           'icon_url': 'https://pbs.twimg.com/profile_images/644169103750512640/zWCOmZLI.png',
           'text': 'test post'
           }
-
 params = urllib.urlencode(params)
-req = urllib2.Request(url)
-req.add_header('Content-Type', 'application/x-www-form-urlencoded')
-req.add_data(params)
-res = urllib2.urlopen(req)
-body = res.read()
 
+def test_post():
+    req = urllib2.Request(url)
+    req.add_header('Content-Type', 'application/x-www-form-urlencoded')
+    req.add_data(params)
+    res = urllib2.urlopen(req)
+    res.read()
+
+
+def main():
+    test_post()
+
+if __name__ == '__main__':
+    main()
